fd51113d9049f55b018029dc23cc0a5258fd9628
Vitalii Grygoruk <vitalii@peakon.com>
2022-09-06 15:29:10 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# ecr-tag-buildkite-plugin
-Buildkite plugin for tagging docker images in AWS ECR
+# ECR Tag Buildkite Plugin
+
+![CI](https://github.com/peakon/ecr-tag-buildkite-plugin/workflows/CI/badge.svg?branch=master)
+
+Retag docker images in AWS ECR based on a lightweight approach described in [AWS documentation](https://docs.aws.amazon.com/AmazonECR/latest/userguide/image-retag.html)
+
+## Example
+
+Add the following to your `pipeline.yml`:
+
+```yml
+steps:
+  - key: tag-docker-image-ecr
+    plugins:
+      - peakon/ecr-tag#v0.0.1:
+          registry-id: ${AWS_ACCOUNT_ID}
+          repository: ${BUILDKITE_PIPELINE_NAME}
+          tag: ${BUILDKITE_COMMIT}
+          new-tags:
+          - ${BUILDKITE_BRANCH}-${BUILDKITE_COMMIT}
+          - ${BUILDKITE_BRANCH}
+```
+## Requirements
+
+- AWS cli, jq
+- AWS IAM Permissions that allow `BatchGetImage` and `PutImage` operations.
+
+## Configuration
+
+| property | description |
+| ---------|-------------|
+| registry-id | ECR registry ID (AWS account id) |
+| repository  | ECR repository name |
+| tag | Existing docker image tag |
+| new-tags | Array of tags to be created |
+
+### AWS profiles
+
+You can specify a custom AWS profile to be used by AWS CLI
+
+- in pipeline YAML (`aws_profile: profile_name`)
+- as `BUILDKITE_PLUGIN_ECR_TAG_AWS_PROFILE` environment variable (e.g. inside agent environment hook).
+
+## Developing
+
+To run the tests:
+
+```shell
+docker-compose run --rm tests
+```
+
+## Contributing
+
+1. Fork the repo
+2. Make the changes
+3. Run shellcheck and plugin lint
+4. Commit and push your changes
+5. Send a pull request
+>>>>>>> 8aa7528 (Initial commit)
---
+++ lib/functions.bash
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+# Reads a list from plugin config into a global result array
+# Returns success if values were read
+plugin_read_list_into_result() {
+  result=()
+
+  for prefix in "$@" ; do
+    local i=0
+    local parameter="${prefix}_${i}"
+
+    if [[ -n "${!prefix:-}" ]] ; then
+      echo "🚨 Plugin received a string for $prefix, expected an array" >&2
+      exit 1
+    fi
+
+    while [[ -n "${!parameter:-}" ]]; do
+      result+=("${!parameter}")
+      i=$((i+1))
+      parameter="${prefix}_${i}"
+    done
+  done
+
+  [[ ${#result[@]} -gt 0 ]] || return 1
+}
+
+function createTag() {
+  local registryId="$1"
+  local repositoryName="$2"
+  local imageTag="$3"
+  local newImageTag="$4"
+  local manifest
+  local result
+  local exit_code
+
+  if [[ -n "${BUILDKITE_PLUGIN_ECR_TAG_AWS_PROFILE:-}" ]]; then
+    aws_cli_args=(--profile "$BUILDKITE_PLUGIN_ECR_TAG_AWS_PROFILE")
+  else
+    aws_cli_args=()
+  fi
+
+  # shellcheck disable=SC2068
+  manifest=$(aws ${aws_cli_args[@]:-} ecr batch-get-image --registry-id "${registryId}" --repository-name "${repositoryName}" --image-ids imageTag="${imageTag}" --output json | jq --raw-output --join-output '.images[0].imageManifest')
+  set +e
+  # shellcheck disable=SC2068
+  result=$( { aws ${aws_cli_args[@]:-} ecr put-image --registry-id "${registryId}" --repository-name "${repositoryName}" --image-tag "${newImageTag}" --image-manifest "${manifest}"; } 2>&1 )
+  exit_code=$?
+  set -e
+  if [ "${exit_code}" -ne 0 ]; then
+    echo "${result}"
+    if [ "${exit_code}" -eq 255 ]; then # https://docs.aws.amazon.com/cli/latest/topic/return-codes.html
+      if [[ "${result}" =~ .*"ImageAlreadyExistsException".* ]]; then
+        echo "Image tag ${newImageTag} already exist. Skipping..."
+      fi
+    else
+      exit "${exit_code}"
+    fi
+  else
+    echo "Successfully retagged image: ${newImageTag}"
+  fi
+}
